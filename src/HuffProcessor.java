23aa56ba18b31d4d96770bb63830f561febdb674
holly-ansel <holly.ansel@duke.edu>
2018-12-02 16:22:50 -0500

    second commit

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -100,8 +100,10 @@ public class HuffProcessor {
 
 	private HuffNode makeTreeFromCounts(int[] counts) {
 		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
-		for(int i = 0; counts[i] > 0; i++) {
-			pq.add(new HuffNode(i, counts[i], null, null));
+		for(int i = 0; i < counts.length; i++) {
+			if(counts[i] > 0) {
+				pq.add(new HuffNode(i, counts[i], null, null));
+			}	
 		}
 		while(pq.size() > 1) {
 			HuffNode left = pq.remove();
